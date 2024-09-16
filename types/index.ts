093272c1efabcade27be6afccc4bf1cd31afc0dc
Arnas Klimasauskas <arnas.klimasauskas@devbridge.com>
2024-09-16 09:57:55 +0300

    Add filter types

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,20 @@ export interface WineCardProps {
   title: string;
   price: number;
 }
+
+export interface filterSelection {
+  key: string;
+  label: string;
+}
+
+type FilterOption = {
+  key: string;
+  label: string;
+};
+
+export interface FilterCardProps {
+  filterIcon: React.ReactNode;
+  filterTitle: string;
+  filterType: string;
+  filterOptions?: FilterOption[];
+}
